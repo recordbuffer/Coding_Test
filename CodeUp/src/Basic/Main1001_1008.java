cecdde2bae7a87912b7f407e7ddd67c57a9445d4
recordbuffer <thal96@kyonggi.ac.kr>
2021-01-29 20:16:58 +0900

    CodeUp

---
+++ CodeUp/src/Basic/Main1001_1008.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class Main1001_1008 {
+	public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello World");
+        System.out.println("Hello\nWorld");
+        System.out.println("\'Hello\'");
+        System.out.println("\"Hello World\"");
+        System.out.println("\"!@#$%^&*()\"");
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+        System.out.printf("\u250C\u252C\u2510\n");
+        System.out.printf("\u251C\u253C\u2524\n");
+        System.out.printf("\u2514\u2534\u2518\n");
+
+	}
+
+}
