cdb9cae1380ba54b884330b606544612639dc65a
recordbuffer <thal96@kyonggi.ac.kr>
2021-02-01 19:45:54 +0900

    codeup

---
+++ CodeUp/src/Basic/io/Main1023.java
@@ -0,0 +1,20 @@
+package Basic.io;
+
+import java.util.Scanner;
+
+public class Main1023 {
+	public static void main(String[] args) {
+		
+		Double x;
+		Scanner sc = new Scanner(System.in);
+		x=sc.nextDouble();
+		
+		String s;
+		s=String.valueOf(x);
+		
+		String [] sr = s.split("\\.");
+		
+		System.out.println(sr[0]+'\n'+sr[1]);
+	}
+
+}
