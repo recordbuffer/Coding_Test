95aaa20f7a006b7144b3010f487cc172fb83a9b8
recordbuffer <thal96@kyonggi.ac.kr>
2021-06-20 23:32:27 +0900

    1025

---
+++ CodeUp/src/Basic/io/Main1025.java
@@ -0,0 +1,27 @@
+package Basic.io;
+
+import java.util.Scanner;
+
+public class Main1025 {
+	public static void main(String[] args) {
+		
+		String s;		
+		Scanner sc = new Scanner(System.in);
+		s=sc.next();
+		
+		int [] data = new int[s.length()];
+				
+		for(int i=0; i<s.length(); i++) {
+			data[i]=s.charAt(i)-'0';
+			
+		}
+		
+		System.out.println("["+data[0]*10000+"]");
+		System.out.println("["+data[1]*1000+"]");
+		System.out.println("["+data[2]*100+"]");
+		System.out.println("["+data[3]*10+"]");
+		System.out.println("["+data[4]+"]");
+		
+	}
+
+}
