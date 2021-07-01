dd67abefe6a4d0ec903cdd6cb0675683efc0d275
recordbuffer <thal96@kyonggi.ac.kr>
2021-07-01 22:01:11 +0900

    decimal convert

---
+++ CodeUp/src/Basic/decimal/Main1036.java
@@ -0,0 +1,15 @@
+package Basic.decimal;
+
+import java.util.Scanner;
+
+public class Main1036 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		char i = sc.nextLine().charAt(0);
+				
+		System.out.printf("%d",i);
+	}
+
+}
