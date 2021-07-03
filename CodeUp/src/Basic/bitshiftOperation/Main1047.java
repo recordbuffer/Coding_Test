d02cbf0563418b4e40ce330c2144a7e72de69179
recordbuffer <thal96@kyonggi.ac.kr>
2021-07-03 21:55:03 +0900

    bitshiftOperation

---
+++ CodeUp/src/Basic/bitshiftOperation/Main1047.java
@@ -0,0 +1,15 @@
+package Basic.bitshiftOperation;
+
+import java.util.Scanner;
+
+public class Main1047 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		System.out.printf("%d",a<<1);
+	}
+
+}
