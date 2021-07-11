e4466879ea2abd93194bbbe3d3c6846ca7d8f246
recordbuffer <thal96@kyonggi.ac.kr>
2021-07-11 20:32:44 +0900

    bitlogic

---
+++ CodeUp/src/basic/bitlogic/Main1059.java
@@ -0,0 +1,14 @@
+package basic.bitlogic;
+
+import java.util.Scanner;
+
+public class Main1059 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		
+		System.out.printf("%d",~s);
+	}
+
+}
