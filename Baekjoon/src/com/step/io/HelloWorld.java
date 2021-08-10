47d328e8c5981a257ffa6e286ed5cec619221a40
recordbuffer <thal96@kyonggi.ac.kr>
2021-08-10 23:49:17 +0900

    백준 1단계

--- Baekjoon/src/com/step/io/HelloWorld.java
+++ Baekjoon/src/com/step/io/HelloWorld.java
@@ -81,10 +81,20 @@ public class HelloWorld {
 	}
 	public static void mul() {
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		String y = sc.nextLine();
 		
+		String one = sc.nextLine();
+		String two = sc.nextLine();
 
+		int t1 = Integer.parseInt(two.substring(2));
+		int t10 = Integer.parseInt(two.substring(1,2));
+		int t100 = Integer.parseInt(two.substring(0,1));		
 		
+		int o = Integer.parseInt(one);		
+		
+		System.out.println(o*t1);
+		System.out.println(o*t10);
+		System.out.println(o*t100);
+
+		System.out.println(((o*t100)*100)+((o*t10)*10)+(o*t1));
 	}
 }
